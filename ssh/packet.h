bb5f42a34e7608dbde2698be394f0d50b2171a16
Markus Friedl <mfriedl@gmail.com>
2012-01-06 13:12:02 +0100

    replace most of opacket.c with #defines

--- ssh/packet.h
+++ ssh/packet.h
@@ -161,7 +161,7 @@ do { \
 int	 ssh_packet_need_rekeying(struct ssh *);
 void	 ssh_packet_set_rekey_limit(struct ssh *, u_int32_t);
 
-/* TODO Hier brauchen wir noch eine Loesung! */
+/* XXX FIXME */
 void	 ssh_packet_backup_state(struct ssh *, struct ssh *);
 void	 ssh_packet_restore_state(struct ssh *, struct ssh *);
 
@@ -169,101 +169,148 @@ void	*ssh_packet_get_input(struct ssh *);
 void	*ssh_packet_get_output(struct ssh *);
 
 /* old API */
-void     packet_set_connection(int, int);
-void     packet_set_timeout(int, int);
-void     packet_set_nonblocking(void);
-int      packet_get_connection_in(void);
-int      packet_get_connection_out(void);
-void     packet_close(void);
-void	 packet_set_encryption_key(const u_char *, u_int, int);
-u_int	 packet_get_encryption_key(u_char *);
-void     packet_set_protocol_flags(u_int);
-u_int	 packet_get_protocol_flags(void);
-void     packet_start_compression(int);
-void     packet_set_interactive(int, int, int);
-int      packet_is_interactive(void);
-void     packet_set_server(void);
-void     packet_set_authenticated(void);
-
-void     packet_start(u_char);
-void     packet_put_char(int ch);
-void     packet_put_int(u_int value);
-void     packet_put_int64(u_int64_t value);
-void     packet_put_bignum(BIGNUM * value);
-void     packet_put_bignum2(BIGNUM * value);
-void     packet_put_ecpoint(const EC_GROUP *, const EC_POINT *);
-void     packet_put_string(const void *buf, u_int len);
-void     packet_put_cstring(const char *str);
-void     packet_put_raw(const void *buf, u_int len);
-void     packet_send(void);
-
-int      packet_read(void);
-void     packet_read_expect(int type);
-int      packet_read_poll(void);
-void     packet_process_incoming(const char *buf, u_int len);
-int      packet_read_seqnr(u_int32_t *seqnr_p);
-int      packet_read_poll_seqnr(u_int32_t *seqnr_p);
-
+extern struct ssh *active_state;
 u_int	 packet_get_char(void);
 u_int	 packet_get_int(void);
-u_int64_t packet_get_int64(void);
-void     packet_get_bignum(BIGNUM * value);
-void     packet_get_bignum2(BIGNUM * value);
-void	 packet_get_ecpoint(const EC_GROUP *, EC_POINT *);
-void	*packet_get_raw(u_int *length_ptr);
-void	*packet_get_string(u_int *length_ptr);
-char	*packet_get_cstring(u_int *length_ptr);
-void	*packet_get_string_ptr(u_int *length_ptr);
-void     packet_disconnect(const char *fmt,...) __attribute__((format(printf, 1, 2)));
-void     packet_send_debug(const char *fmt,...) __attribute__((format(printf, 1, 2)));
-
-void	 set_newkeys(int mode);
-int	 packet_get_keyiv_len(int);
-void	 packet_get_keyiv(int, u_char *, u_int);
-int	 packet_get_keycontext(int, u_char *);
-void	 packet_set_keycontext(int, u_char *);
-void	 packet_get_state(int, u_int32_t *, u_int64_t *, u_int32_t *, u_int64_t *);
-void	 packet_set_state(int, u_int32_t, u_int64_t, u_int32_t, u_int64_t);
-int	 packet_get_ssh1_cipher(void);
-void	 packet_set_iv(int, u_char *);
-void	*packet_get_newkeys(int);
-
-void     packet_write_poll(void);
-void     packet_write_wait(void);
-int      packet_have_data_to_write(void);
-int      packet_not_very_much_data_to_write(void);
-
-int	 packet_connection_is_on_socket(void);
-int	 packet_remaining(void);
-void	 packet_send_ignore(int);
-void	 packet_add_padding(u_char);
-
-void	 tty_make_modes(int, struct termios *);
-void	 tty_parse_modes(int, int *);
-
-void	 packet_set_alive_timeouts(int);
-int	 packet_inc_alive_timeouts(void);
-int	 packet_set_maxsize(u_int);
-u_int	 packet_get_maxsize(void);
-
-/* don't allow remaining bytes after the end of the message */
-#define packet_check_eom() \
-do { \
-	int _len = packet_remaining(); \
-	if (_len > 0) { \
-		logit("Packet integrity error (%d bytes remaining) at %s:%d", \
-		    _len ,__FILE__, __LINE__); \
-		packet_disconnect("Packet integrity error."); \
-	} \
-} while (0)
-
-int	 packet_need_rekeying(void);
-void	 packet_set_rekey_limit(u_int32_t);
-
 void	 packet_backup_state(void);
 void	 packet_restore_state(void);
-
-void	*packet_get_input(void);
-void	*packet_get_output(void);
+#define packet_set_connection(fd_in, fd_out) \
+	ssh_packet_set_connection(active_state, (fd_in), (fd_out))
+#define packet_set_timeout(timeout, count) \
+	ssh_packet_set_timeout(active_state, (timeout), (count))
+#define packet_connection_is_on_socket() \
+	ssh_packet_connection_is_on_socket(active_state)
+#define packet_get_keyiv(mode, iv, len) \
+	ssh_packet_get_keyiv(active_state, (mode), (iv), (len))
+#define packet_get_keycontext(mode, dat) \
+	ssh_packet_get_keycontext(active_state, (mode), (dat))
+#define packet_set_keycontext(mode, dat) \
+	ssh_packet_set_keycontext(active_state, (mode), (dat))
+#define packet_get_keyiv_len(mode) \
+	ssh_packet_get_keyiv_len(active_state, (mode))
+#define packet_set_iv(mode, dat) \
+	ssh_packet_set_iv(active_state, (mode), (dat))
+#define packet_get_ssh1_cipher() \
+	ssh_packet_get_ssh1_cipher(active_state)
+#define packet_get_state(mode, seqnr, blocks, packets, bytes) \
+	ssh_packet_get_state(active_state, (mode), (seqnr), (blocks), (packets), (bytes))
+#define packet_set_state(mode, seqnr, blocks, packets, bytes) \
+	ssh_packet_set_state(active_state, (mode), (seqnr), (blocks), (packets), (bytes))
+#define packet_set_nonblocking() \
+	ssh_packet_set_nonblocking(active_state)
+#define packet_get_connection_in() \
+	ssh_packet_get_connection_in(active_state)
+#define packet_get_connection_out() \
+	ssh_packet_get_connection_out(active_state)
+#define packet_close() \
+	ssh_packet_close(active_state)
+#define packet_set_protocol_flags(protocol_flags) \
+	ssh_packet_set_protocol_flags(active_state, (protocol_flags))
+#define packet_get_protocol_flags() \
+	ssh_packet_get_protocol_flags(active_state)
+#define packet_start_compression(level) \
+	ssh_packet_start_compression(active_state, (level))
+#define packet_set_encryption_key(key, keylen, number) \
+	ssh_packet_set_encryption_key(active_state, (key), (keylen), (number))
+#define packet_get_encryption_key(key) \
+	ssh_packet_get_encryption_key(active_state, (key))
+#define packet_start(type) \
+	ssh_packet_start(active_state, (type))
+#define packet_put_char(value) \
+	ssh_packet_put_char(active_state, (value))
+#define packet_put_int(value) \
+	ssh_packet_put_int(active_state, (value))
+#define packet_put_int64(value) \
+	ssh_packet_put_int64(active_state, (value))
+#define packet_put_string( buf, len) \
+	ssh_packet_put_string(active_state, (buf), (len))
+#define packet_put_cstring(str) \
+	ssh_packet_put_cstring(active_state, (str))
+#define packet_put_raw(buf, len) \
+	ssh_packet_put_raw(active_state, (buf), (len))
+#define packet_put_bignum(value) \
+	ssh_packet_put_bignum(active_state, (value))
+#define packet_put_bignum2(value) \
+	ssh_packet_put_bignum2(active_state, (value))
+#define packet_put_ecpoint(curve, point) \
+	ssh_packet_put_ecpoint(active_state, (curve), (point))
+#define packet_send() \
+	ssh_packet_send(active_state)
+#define packet_read_seqnr(seqnr_p) \
+	ssh_packet_read_seqnr(active_state, (seqnr_p))
+#define packet_read() \
+	ssh_packet_read(active_state)
+#define packet_read_expect(expected_type) \
+	ssh_packet_read_expect(active_state, (expected_type))
+#define packet_read_poll_seqnr(seqnr_p) \
+	ssh_packet_read_poll_seqnr(active_state, (seqnr_p))
+#define packet_read_poll() \
+	ssh_packet_read_poll(active_state)
+#define packet_process_incoming(buf, len) \
+	ssh_packet_process_incoming(active_state, (buf), (len))
+#define packet_get_int64() \
+	ssh_packet_get_int64(active_state)
+#define packet_get_bignum(value) \
+	ssh_packet_get_bignum(active_state, (value))
+#define packet_get_bignum2(value) \
+	ssh_packet_get_bignum2(active_state, (value))
+#define packet_get_ecpoint(curve, point) \
+	ssh_packet_get_ecpoint(active_state, (curve), (point))
+#define packet_get_raw(length_ptr) \
+	ssh_packet_get_raw(active_state, (length_ptr))
+#define packet_remaining() \
+	ssh_packet_remaining(active_state)
+#define packet_get_string(length_ptr) \
+	ssh_packet_get_string(active_state, (length_ptr))
+#define packet_get_string_ptr(length_ptr) \
+	ssh_packet_get_string_ptr(active_state, (length_ptr))
+#define packet_get_cstring(length_ptr) \
+	ssh_packet_get_cstring(active_state, (length_ptr))
+#define packet_send_debug(fmt, args...) \
+	ssh_packet_send_debug(active_state, (fmt), ##args)
+#define packet_disconnect(fmt, args...) \
+	ssh_packet_disconnect(active_state, (fmt), ##args)
+#define packet_write_poll() \
+	ssh_packet_write_poll(active_state)
+#define packet_write_wait() \
+	ssh_packet_write_wait(active_state)
+#define packet_have_data_to_write() \
+	ssh_packet_have_data_to_write(active_state)
+#define packet_not_very_much_data_to_write() \
+	ssh_packet_not_very_much_data_to_write(active_state)
+#define packet_set_interactive(interactive, qos_interactive, qos_bulk) \
+	ssh_packet_set_interactive(active_state, (interactive), (qos_interactive), (qos_bulk))
+#define packet_is_interactive() \
+	ssh_packet_is_interactive(active_state)
+#define packet_set_maxsize(s) \
+	ssh_packet_set_maxsize(active_state, (s))
+#define packet_inc_alive_timeouts() \
+	ssh_packet_inc_alive_timeouts(active_state)
+#define packet_set_alive_timeouts(ka) \
+	ssh_packet_set_alive_timeouts(active_state, (ka))
+#define packet_get_maxsize() \
+	ssh_packet_get_maxsize(active_state)
+#define packet_add_padding(pad) \
+	ssh_packet_add_padding(active_state, (pad))
+#define packet_send_ignore(nbytes) \
+	ssh_packet_send_ignore(active_state, (nbytes))
+#define packet_need_rekeying() \
+	ssh_packet_need_rekeying(active_state)
+#define packet_set_rekey_limit(bytes) \
+	ssh_packet_set_rekey_limit(active_state, (bytes))
+#define packet_set_server() \
+	ssh_packet_set_server(active_state)
+#define packet_set_authenticated() \
+	ssh_packet_set_authenticated(active_state)
+#define packet_get_input() \
+	ssh_packet_get_input(active_state)
+#define packet_get_output() \
+	ssh_packet_get_output(active_state)
+#define packet_get_newkeys(mode) \
+	ssh_packet_get_newkeys(active_state, (mode))
+#define packet_check_eom() \
+	ssh_packet_check_eom(active_state)
+#define set_newkeys(mode) \
+	ssh_set_newkeys(active_state, (mode))
 
 #endif				/* PACKET_H */
