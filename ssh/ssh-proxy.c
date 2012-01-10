81e3938108d268a55c1f942d1b2b3f8fb380f97a
Markus Friedl <mfriedl@gmail.com>
2012-01-10 22:51:05 +0100

    add a libevent based ssh proxy
    example usage:
    % test -f /tmp/hk2 || ssh-keygen -N '' -t ecdsa -f  /tmp/hk2
    % ssh-keyscan -t ecdsa 127.0.0.1 | sed 's/^[^ ]* //'  > /tmp/hk.pub
    % ./ssh-proxy/obj/ssh-proxy -f -S /tmp/hk2 -C /tmp/hk.pub -L 127.0.0.1:12345:127.0.0.1:22 -d
    % ssh -o hostkeyalgorithms'='ecdsa-sha2-nistp256 -o hostkeyalias'='egal2 -v 127.0.0.1 -p 12345

--- ssh/ssh-proxy.c
+++ ssh/ssh-proxy.c
@@ -1,6 +1,6 @@
-/* $OpenBSD: sftp-server.c,v 1.94 2011/06/17 21:46:16 djm Exp $ */
+/* $OpenBSD: $ */
 /*
- * Copyright (c) 2000-2004 Markus Friedl.  All rights reserved.
+ * Copyright (c) 2012 Markus Friedl.  All rights reserved.
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -16,71 +16,359 @@
  */
 
 #include <sys/types.h>
+#include <sys/queue.h>
 #include <sys/socket.h>
+
+#include <errno.h>
+#include <event.h>
+#include <fcntl.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <errno.h>
-#include <fcntl.h>
 
 #include "ssh_api.h"
 #include "xmalloc.h"
-#include "buffer.h"
 #include "log.h"
 #include "misc.h"
 #include "myproposal.h"
+#include "readconf.h"
 
-static void
-connect_to_server(char *path, char **args, int *in, int *out)
+struct side {
+	int fd;
+	struct event input, output;
+	struct ssh *ssh;
+};
+struct session {
+	struct side client, server;
+	int connected;
+	TAILQ_ENTRY(session) next;
+};
+Forward fwd;
+
+void accept_cb(int, short, void *);
+void connect_cb(int, short, void *);
+void input_cb(int, short, void *);
+void output_cb(int, short, void *);
+
+int do_connect(const char *, int);
+int do_listen(const char *, int);
+void session_close(struct session *);
+void ssh_packet_fwd(struct side *, struct side *);
+void usage(void);
+
+uid_t original_real_uid;	/* XXX */
+TAILQ_HEAD(, session) sessions;
+struct kex_params kex_params;
+
+#define BUFSZ 16*1024
+char keybuf[BUFSZ];
+char known_keybuf[BUFSZ];
+
+int
+do_listen(const char *addr, int port)
 {
-	int c_in, c_out;
-	pid_t pid;
+	int sock, on = 1;
+	struct addrinfo hints, *ai, *aitop;
+	char strport[NI_MAXSERV];
+	snprintf(strport, sizeof strport, "%d", port);
 
-	int inout[2];
-
-	if (socketpair(AF_UNIX, SOCK_STREAM, 0, inout) == -1)
-		fatal("socketpair: %s", strerror(errno));
-	*in = *out = inout[0];
-	c_in = c_out = inout[1];
-
-	if ((pid = fork()) == -1)
-		fatal("fork: %s", strerror(errno));
-	else if (pid == 0) {
-		if ((dup2(c_in, STDIN_FILENO) == -1) ||
-		    (dup2(c_out, STDOUT_FILENO) == -1)) {
-			fprintf(stderr, "dup2: %s\n", strerror(errno));
-			_exit(1);
-		}
-		close(*in);
-		close(*out);
-		close(c_in);
-		close(c_out);
-
-		/*
-		 * The underlying ssh is in the same process group, so we must
-		 * ignore SIGINT if we want to gracefully abort commands,
-		 * otherwise the signal will make it to the ssh process and
-		 * kill it too.  Contrawise, since sftp sends SIGTERMs to the
-		 * underlying ssh, it must *not* ignore that signal.
-		 */
-		signal(SIGINT, SIG_IGN);
-		signal(SIGTERM, SIG_DFL);
-		execvp(path, args);
-		fprintf(stderr, "exec: %s: %s\n", path, strerror(errno));
-		_exit(1);
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	if (getaddrinfo(addr, strport, &hints, &aitop) != 0) {
+		error("getaddrinfo: fatal error");
+		return -1;
 	}
-
-	close(c_in);
-	close(c_out);
+	sock = -1;
+	for (ai = aitop; ai; ai = ai->ai_next) {
+		if (ai->ai_family != AF_INET && ai->ai_family != AF_INET6)
+			continue;
+		if ((sock = socket(ai->ai_family, SOCK_STREAM, 0)) < 0)
+			continue;
+		if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &on,
+		    sizeof(on)) == -1) {
+			error("setsockopt: %s", strerror(errno));
+			close(sock);
+			sock = -1;
+			continue;
+		}
+		if (bind(sock, ai->ai_addr, ai->ai_addrlen) < 0) {
+			error("bind: %s", strerror(errno));
+			close(sock);
+			sock = -1;
+			continue;
+		}
+		if (listen(sock, 5) < 0) {
+			error("listen: %s", strerror(errno));
+			close(sock);
+			sock = -1;
+			continue;
+		}
+		break;
+	}
+	freeaddrinfo(aitop);
+	return sock;
 }
 
-static void
+int
+do_connect(const char *addr, int port)
+{
+	struct addrinfo hints, *ai, *aitop;
+	int gaierr;
+	int sock = -1;
+	char strport[NI_MAXSERV];
+	snprintf(strport, sizeof strport, "%d", port);
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	if ((gaierr = getaddrinfo(addr, strport, &hints, &aitop)) != 0) {
+		error("getaddrinfo %s: %s", addr, gai_strerror(gaierr));
+		return -1;
+	}
+	for (ai = aitop; ai; ai = ai->ai_next) {
+		if (ai->ai_family != AF_INET && ai->ai_family != AF_INET6)
+			continue;
+		if ((sock = socket(ai->ai_family, SOCK_STREAM, 0)) < 0) {
+			error("socket: %s", strerror(errno));
+			continue;
+		}
+		if (fcntl(sock, F_SETFL, O_NONBLOCK) < 0) {
+			error("fcntl connect F_SETFL: %s", strerror(errno));
+			close(sock);
+			sock = -1;
+			continue;
+		}
+		if (connect(sock, ai->ai_addr, ai->ai_addrlen) < 0 &&
+		    errno != EINPROGRESS) {
+			error("connect(%s, %d): %s", addr, port, strerror(errno));
+			close(sock);
+			sock = -1;
+			continue;
+		}
+		break;
+	}
+	freeaddrinfo(aitop);
+	if (!ai)
+		return -1;
+	return sock;
+}
+
+void
+session_close(struct session *s)
+{
+	if (s->client.fd != -1)
+		close(s->client.fd);
+	if (s->server.fd != -1)
+		close(s->server.fd);
+	if (s->connected == 1) {
+		event_del(&s->client.input);
+		event_del(&s->client.output);
+		event_del(&s->server.input);
+		event_del(&s->server.output);
+		if (s->client.ssh) {
+			/* XXX free */
+			s->client.ssh = NULL;
+		}
+		if (s->server.ssh) {
+			/* XXX free */
+			s->server.ssh = NULL;
+		}
+		TAILQ_REMOVE(&sessions, s, next);
+	}
+	xfree(s);
+}
+
+void
+accept_cb(int fd, short type, void *arg)
+{
+	struct event *ev = arg;
+	struct session *s;
+	socklen_t addrlen;
+	struct sockaddr addr;
+
+	s = xcalloc(1, sizeof(struct session));
+	s->connected = 0;
+	s->server.fd = -1;
+	s->client.fd = -1;
+	s->server.ssh = NULL;
+	s->client.ssh = NULL;
+	event_add(ev, NULL);
+	addrlen = sizeof(addr);
+	if ((s->client.fd = accept(fd, &addr, &addrlen)) < 0) {
+		fatal("accept: %s", strerror(errno));
+		goto fail;
+	}
+	if (fcntl(s->client.fd, F_SETFL, O_NONBLOCK) < 0) {
+		error("fcntl accepted F_SETFL: %s", strerror(errno));
+		goto fail;
+	}
+	if ((s->server.fd = do_connect(fwd.connect_host, fwd.connect_port)) < 0) {
+		error("do_connect() failed");
+		goto fail;
+	}
+	event_set(&s->server.output, s->server.fd, EV_WRITE, connect_cb, s);
+	event_add(&s->server.output, NULL);
+	return;
+fail:
+	if (s) {
+		if (s->client.fd != -1)
+			close(s->client.fd);
+		xfree(s);
+	}
+}
+
+void
+connect_cb(int fd, short type, void *arg)
+{
+	struct session *s = arg;
+	int soerr;
+	socklen_t sz = sizeof(soerr);
+
+	event_del(&s->server.output);
+	if (getsockopt(s->server.fd, SOL_SOCKET, SO_ERROR, &soerr, &sz) < 0) {
+		soerr = errno;
+		error("connect_cb: getsockopt: %s", strerror(errno));
+	}
+	if (soerr != 0) {
+		close(s->server.fd);
+		s->server.fd = -1;
+		session_close(s);
+		return;
+	}
+	event_set(&s->client.input,  s->client.fd, EV_READ, input_cb, s);
+	event_set(&s->client.output, s->client.fd, EV_WRITE, output_cb, s);
+	event_set(&s->server.input,  s->server.fd, EV_READ, input_cb, s);
+	event_set(&s->server.output, s->server.fd, EV_WRITE, output_cb, s);
+	memcpy(kex_params.proposal, myproposal, sizeof(kex_params.proposal));
+	s->client.ssh = ssh_init(1, &kex_params);
+	s->server.ssh = ssh_init(0, &kex_params);
+	if (ssh_add_hostkey(s->client.ssh, keybuf) < 0)
+		fatal("could not load server hostkey");
+	if (ssh_add_hostkey(s->server.ssh, known_keybuf) < 0)
+		fatal("could not load client hostkey");
+	event_add(&s->server.input, NULL);
+	event_add(&s->client.input, NULL);
+	s->connected = 1;
+	TAILQ_INSERT_TAIL(&sessions, s, next);
+}
+
+void
+ssh_packet_fwd(struct side *from, struct side *to)
+{
+	int type;
+	u_char *data;
+	u_int len;
+
+	if (!from->ssh || !to->ssh)
+		return;
+	type = ssh_packet_get(from->ssh);
+	if (type) {
+		data = ssh_packet_payload(from->ssh, &len);
+		debug("ssh_packet_fwd %d->%d type %d len %d",
+		    from->fd, to->fd, type, len);
+		ssh_packet_put(to->ssh, type, data, len);
+	} else {
+		debug3("no packet on %d", from->fd);
+	}
+	ssh_output_ptr(from->ssh, &len);
+	if (len) {
+		debug3("output %d for %d", len, from->fd);
+		event_add(&from->output, NULL);
+	}
+	ssh_output_ptr(to->ssh, &len);
+	if (len) {
+		debug3("output %d for %d", len, to->fd);
+		event_add(&to->output, NULL);
+	}
+}
+
+void
+input_cb(int fd, short type, void *arg)
+{
+	u_char buf[BUFSZ];
+	struct session *s = arg;
+	struct side *r, *w;
+	ssize_t len;
+	const char *tag;
+
+	if (fd == s->client.fd) {
+		tag = "client";
+		r = &s->client;
+		w = &s->server;
+	} else {
+		tag = "server";
+		r = &s->server;
+		w = &s->client;
+	}
+	debug2("input_cb %s fd %d", tag, fd);
+	len = read(fd, buf, sizeof(buf));
+	if (len < 0 && (errno == EINTR || errno == EAGAIN)) {
+		event_add(&r->input, NULL);
+	} else if (len <= 0) {
+		debug("read %s failed fd %d len %zd", tag, fd, len);
+		session_close(s);
+		return;
+	} else {
+		debug2("read %s fd %d len %zd", tag, fd, len);
+		event_add(&r->input, NULL);
+		ssh_input_append(r->ssh, buf, len);
+	}
+	ssh_packet_fwd(r, w);
+	ssh_packet_fwd(w, r);
+}
+
+void
+output_cb(int fd, short type, void *arg)
+{
+	struct session *s = arg;
+	struct side *r, *w;
+	ssize_t len;
+	const char *tag;
+	ssize_t olen;
+	char *obuf;
+
+	if (fd == s->client.fd) {
+		tag = "client";
+		w = &s->client;
+		r = &s->server;
+	} else {
+		tag = "server";
+		w = &s->server;
+		r = &s->client;
+	}
+	debug2("output_cb %s fd %d", tag, fd);
+	obuf = ssh_output_ptr(w->ssh, (u_int *)&olen);
+	if (olen > 0) {
+		len = write(fd, obuf, olen);
+		if (len < 0 && (errno == EINTR || errno == EAGAIN)) {
+			event_add(&w->output, NULL);
+		} else if (len <= 0) {
+			debug("write %s failed fd %d len %zd", tag, fd, len);
+			session_close(s);
+			return;
+		} else if (len < olen) {
+			debug("write %s partial fd %d len %zd olen %zd",
+			    tag, fd, len, olen);
+			ssh_output_consume(w->ssh, len);
+		} else {
+			debug2("write %s done fd %d", tag, fd);
+			ssh_output_consume(w->ssh, len);
+		}
+	}
+	ssh_packet_fwd(r, w);
+	ssh_packet_fwd(w, r);
+}
+
+void
 usage(void)
 {
 	extern char *__progname;
 
 	fprintf(stderr,
-	    "usage: %s [-s] [-h hostkey] [-l log_level] [-f log_facility]\n",
+	    "usage: %s [-dfh] [-L [laddr:]lport:saddr:sport]"
+	    " [-C knownkey] [-S serverkey]\n",
 	    __progname);
 	exit(1);
 }
@@ -88,163 +376,72 @@ usage(void)
 int
 main(int argc, char **argv)
 {
-	fd_set *rset, *wset;
-	int in, out, max, ch, type, fd, skipargs = 0, log_stderr = 1;
-	int send_ignore = 0, userauth_sent = 0;
-	int is_server = 0;
-	ssize_t len, olen, set_size;
+	int ch, log_stderr = 1, foreground = 0, fd;
+	struct event ev;
+	ssize_t len;
+	char *hostkey_file = NULL, *known_hostkey_file = NULL;
 	SyslogFacility log_facility = SYSLOG_FACILITY_AUTH;
-	LogLevel log_level = SYSLOG_LEVEL_DEBUG3;
-	char buf[4*4096];	/* shrink in order to trigger bugs */
-	char keybuf[8*1024];
-	char *obuf, *hostkey_file = NULL;
-	struct ssh *ssh;
-	struct kex_params kex_params;
-
-	extern char *optarg;
+	LogLevel log_level = SYSLOG_LEVEL_VERBOSE;
 	extern char *__progname;
 
-	log_init(__progname, log_level, log_facility, log_stderr);
+	TAILQ_INIT(&sessions);
 
-	while (!skipargs && (ch = getopt(argc, argv, "ef:il:h:s")) != -1) {
+	while ((ch = getopt(argc, argv, "dfC:L:S:")) != -1) {
 		switch (ch) {
-		case 'e':
-			log_stderr = 1;
+		case 'd':
+			if (log_level == SYSLOG_LEVEL_VERBOSE)
+				log_level = SYSLOG_LEVEL_DEBUG1;
+			else if (log_level == SYSLOG_LEVEL_DEBUG1)
+				log_level = SYSLOG_LEVEL_DEBUG2;
+			else if (log_level == SYSLOG_LEVEL_DEBUG2)
+				log_level = SYSLOG_LEVEL_DEBUG3;
 			break;
 		case 'f':
-			log_stderr = 0;
-			log_facility = log_facility_number(optarg);
-			if (log_facility == SYSLOG_FACILITY_NOT_SET)
-				error("Invalid log facility \"%s\"", optarg);
+			foreground = 1;
 			break;
-		case 'h':
+		case 'C':
+			known_hostkey_file = optarg;
+			break;
+		case 'L':
+			if (parse_forward(&fwd, optarg, 0, 0) == 0)
+				fatal("cannot parse: %s", optarg);
+			if (fwd.listen_host == NULL)
+				fwd.listen_host = "0.0.0.0";
+			break;
+		case 'S':
 			hostkey_file = optarg;
-			break;
-		case 'i':
-			send_ignore = 10;
-			break;
-		case 's':
-			is_server = 1;
-			break;
-		case 'l':
-			log_stderr = 0;
-			log_level = log_level_number(optarg);
-			if (log_level == SYSLOG_LEVEL_NOT_SET)
-				error("Invalid log level \"%s\"", optarg);
 			break;
 		default:
 			usage();
+			break;
 		}
 	}
-
 	log_init(__progname, log_level, log_facility, log_stderr);
-
-	if (is_server) {
-		in = STDIN_FILENO;
-		out = STDOUT_FILENO;
-	} else
-		connect_to_server(argv[optind], argv+optind, &in, &out);
-
-	max = 0;
-	if (in > max)
-		max = in;
-	if (out > max)
-		max = out;
-
-	set_size = howmany(max + 1, NFDBITS) * sizeof(fd_mask);
-	rset = (fd_set *)xmalloc(set_size);
-	wset = (fd_set *)xmalloc(set_size);
-
-	memcpy(kex_params.proposal, myproposal, sizeof(kex_params.proposal));
-#if 0
-	kex_params.proposal[PROPOSAL_SERVER_HOST_KEY_ALGS] = "ssh-rsa";
-#endif
-	ssh = ssh_init(is_server, &kex_params);
-
 	if (hostkey_file) {
 		if ((fd = open(hostkey_file, O_RDONLY, 0)) < 0)
 			fatal("open: %s %s", hostkey_file, strerror(errno));
 		if ((len = read(fd, keybuf, sizeof(keybuf))) < 0)
 			fatal("read: %s %s", hostkey_file, strerror(errno));
 		keybuf[len] = '\0';
-		if (ssh_add_hostkey(ssh, keybuf) < 0)
-			fatal("could not load hostkey");
-		bzero(keybuf, sizeof(keybuf));
+		debug("hk: read %zd bytes", len);
+		close(fd);
 	}
-
-	for (;;) {
-		/* Process requests packets */
-		type = ssh_packet_get(ssh);
-		debug("got message type %d session id len %d done %d",
-		    type, ssh->kex->session_id_len, ssh->kex->done);
-
-		/* send some test messages */
-		if (ssh->kex->done) {
-			while (send_ignore) {
-				ssh_packet_start(ssh, SSH2_MSG_IGNORE);
-				ssh_packet_put_cstring(ssh, "markus");
-				ssh_packet_send(ssh);
-				send_ignore--;
-			}
-			if (is_server) {
-			} else if (!userauth_sent) {
-				ssh_packet_start(ssh, SSH2_MSG_SERVICE_REQUEST);
-				ssh_packet_put_cstring(ssh, "ssh-userauth");
-				ssh_packet_send(ssh);
-				userauth_sent = 1;
-			} else if (type == SSH2_MSG_SERVICE_ACCEPT) {
-				debug("got a service accept");
-			}
-		}
-
-		memset(rset, 0, set_size);
-		memset(wset, 0, set_size);
-
-		/*
-		 * Ensure that we can read a full buffer and handle
-		 * the worst-case length packet it can generate,
-		 * otherwise apply backpressure by stopping reads.
-		 */
-		if (ssh_input_space(ssh, sizeof(buf)) &&
-		    ssh_output_space(ssh, 32*1024))
-			FD_SET(in, rset);
-
-		obuf = ssh_output_ptr(ssh, (u_int *)&olen);
-		if (olen > 0) {
-			debug("olen: %lu", olen);
-			FD_SET(out, wset);
-		}
-
-		if (select(max+1, rset, wset, NULL, NULL) < 0) {
-			if (errno == EINTR)
-				continue;
-			error("select: %s", strerror(errno));
-			exit(2);
-		}
-
-		/* copy stdin to input */
-		if (FD_ISSET(in, rset)) {
-			len = read(in, buf, sizeof buf);
-			if (len == 0) {
-				debug("read eof");
-				exit(0);
-			} else if (len < 0) {
-				error("read: %s", strerror(errno));
-				exit(1);
-			} else {
-				ssh_input_append(ssh, buf, len);
-			}
-		}
-		/* send output to stdout */
-		if (FD_ISSET(out, wset)) {
-			len = write(out, obuf, olen);
-			if (len < 0) {
-				error("write: %s", strerror(errno));
-				exit(1);
-			} else {
-				ssh_output_consume(ssh, len);
-			}
-		}
-
+	if (known_hostkey_file) {
+		if ((fd = open(known_hostkey_file, O_RDONLY, 0)) < 0)
+			fatal("open: %s %s", hostkey_file, strerror(errno));
+		if ((len = read(fd, known_keybuf, sizeof(known_keybuf))) < 0)
+			fatal("kh: read: %s %s", hostkey_file, strerror(errno));
+		known_keybuf[len] = '\0';
+		debug("kh: read %zd bytes", len);
+		close(fd);
 	}
+	if (!foreground)
+		daemon(0, 0);
+	event_init();
+	if ((fd = do_listen(fwd.listen_host, fwd.listen_port)) < 0)
+		fatal(" do_listen failed");
+	event_set(&ev, fd, EV_READ, accept_cb, &ev);
+	event_add(&ev, NULL);
+	event_dispatch();
+	exit(1);
 }
