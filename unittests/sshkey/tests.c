b5dbccb6fa53e3ff4b572467d292c06e8c719186
Damien Miller <djm@mindrot.org>
2012-01-12 23:05:51 +1100

    start regress tests for sshkey code

---
+++ unittests/sshkey/tests.c
@@ -0,0 +1,16 @@
+/* 	$OpenBSD$ */
+/*
+ * Regress test for sshbuf.h buffer API
+ *
+ * Placed in the public domain
+ */
+
+#include "test_helper.h"
+
+void sshkey_tests(void);
+
+void
+tests(void)
+{
+	sshkey_tests();
+}
