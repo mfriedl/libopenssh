d9fd38c3a23f03729f500e34282d1ec3688e4224
Markus Friedl <mfriedl@gmail.com>
2012-02-13 21:24:36 +0100

    unbreak privsep by resetting the rekeying flag after authentication for now

--- ssh/packet.c
+++ ssh/packet.c
@@ -2489,6 +2489,7 @@ ssh_packet_set_postauth(struct ssh *ssh)
 	debug("%s: called", __func__);
 	/* This was set in net child, but is not visible in user child */
 	ssh->state->after_authentication = 1;
+	ssh->state->rekeying = 0;
 	for (mode = 0; mode < MODE_MAX; mode++) {
 		if (ssh->state->newkeys[mode] == NULL)
 			continue;
