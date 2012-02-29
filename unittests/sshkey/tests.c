38fe4588924afbea5552226e05c34dfb4647d6e3
Damien Miller <djm@mindrot.org>
2012-02-29 14:27:26 +1100

    hook in all the tests

--- unittests/sshkey/tests.c
+++ unittests/sshkey/tests.c
@@ -5,12 +5,19 @@
  * Placed in the public domain
  */
 
+#include <openssl/evp.h>
+
 #include "test_helper.h"
 
 void sshkey_tests(void);
+void sshkey_file_tests(void);
 
 void
 tests(void)
 {
+	OpenSSL_add_all_algorithms();
+	ERR_load_CRYPTO_strings();
+
 	sshkey_tests();
+	sshkey_file_tests();
 }
